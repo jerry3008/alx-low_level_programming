9e58e20d83900fa2d5155a2afaa845e7cb444f23
jerry3008 <gbemuduj@gmail>
2023-01-16 14:47:00 -0800

    corrected task 0

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -3,7 +3,7 @@
 
 /*
  * File: 0-object_like_macro.h
- * Author: Raymond Omoraka
+ * Author: Gbemudu Jerry
  * Desc: Header file that defines a macro named
  *       SIZE as an abbreviation for the token 1024.
  */
